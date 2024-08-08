ab01c37877576f499ab95bf23d0a3a2c8ded347d
Anyl Pascal <anylpascal@ProLuve.local>
2024-08-08 06:50:34 -0400

    contact form test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import ContactForm from "@/components/ContactForm";
+
 
 export default function Home() {
   return (
@@ -22,6 +24,12 @@ export default function Home() {
               height={24}
               priority
             />
+            <div className="p-4 max-w-3xl mx-auto">
+              <h1 className="text-3xl font-bold">Contact Us</h1>
+              <p>Please fill in the form below</p>
+
+              <ContactForm />
+            </div>
           </a>
         </div>
       </div>
