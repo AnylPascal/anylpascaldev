ea0badda130f85bc5ecd416db919cad3b39929fe
Anyl Pascal <anylpascal@ProLuve.local>
2024-08-14 11:24:58 -0400

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anyl Pascal",
-  description: "Software Engineer",
+  title: "Anyl Pascal Software Developer",
+  description: "Engineer | Pen Testing Portfolio",
 };
 
 export default function RootLayout({
